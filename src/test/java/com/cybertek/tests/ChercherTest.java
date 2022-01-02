36b5a69be2f5b59e728d587bc6eb1bad7ea3db8d
EU7Git&Github <firatgzl95@gmail.com>
2022-01-02 18:27:45 +0300

    First Commit

---
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -0,0 +1,44 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.WebDriveFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChercherTest {
+/*
+    Task1:
+    1. Go to https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver
+    2. Click on "Click me, to Open an alert after 5 seconds"
+    3. Explicitly wait until alert is present
+    4. Then handle the Javascript alert
+*/
+    WebDriver driver;// declare our reference for the object
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriveFactory.getDriver("chrome"); //create the object
+        driver.manage().window().maximize();
+        //implicitly wait,this is going to be applied to whole test cases and elements
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+
+    }
+
+    @Test
+    public void alertPresentTest(){
+
+
+
+
+    }
+
+}
